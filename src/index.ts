dd8f612d3f093ad4191ec61d243dffd345736fcd
InkoHX <inkohx@gmail.com>
2020-06-05 00:25:34 +0900

    update index.js

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
-import { AwaitMessagesOptions, Client, Message } from 'discord.js'
-import { highlightAuto } from 'highlight.js'
+import { AwaitMessagesOptions, Client, Collection, Message } from 'discord.js'
+import { highlightAuto, listLanguages } from 'highlight.js'
 
 const client = new Client()
 
 client.once('ready', () => console.log('READY'))
 
 const commandName = '>highlight'
+const languageSubset = listLanguages().filter(value => value !== 'livecodeserver')
 const awaitMessageOptions: AwaitMessagesOptions = {
   max: 1,
   time: 30000,
   errors: ['time']
 }
 
-const handleMessageHighlight = async (message: Message): Promise<Message> => {
-  const target = await message.channel.messages.fetch(message.content).catch(() => null)
-  if (!target) return message.reply('対象のメッセージが取得できませんでした。IDを間違えている可能性があります。')
-  const languageCode = highlightAuto(target.content, ['typescript', 'javascript']).language
+const sendHighlightedCode = async (targetMessage: Message, executorMessage: Message): Promise<Message> => {
+  if (targetMessage.author.bot || targetMessage.system) return executorMessage.reply('ボットまたはシステムが送信したメッセージはハイライトできません。')
+  const code = highlightAuto(targetMessage.content, languageSubset).language
 
-  console.log(languageCode)
-
-  return message.channel.send(target.content, { code: languageCode, split: true })
+  return executorMessage.reply(targetMessage.content, { code })
 }
+
+// eslint-disable-next-line max-len
+const awaitMessages = async (message: Message): Promise<Message> => message.channel.awaitMessages((target: Message) => target.author.id === message.author.id, awaitMessageOptions)
+  .then(collected => collected.first() as Message)
+  .then(target => message.channel.messages.fetch(target.content))
+  .then(target => sendHighlightedCode(target, message))
 
 // eslint-disable-next-line @typescript-eslint/no-misused-promises
 client.on('message', async message => {
@@ -28,13 +32,20 @@ client.on('message', async message => {
   if (author.bot || message.system) return
   if (!message.content.startsWith(commandName)) return
 
-  await message.reply('ハイライトするメッセージのIDを送信してください。')
-    .then(sendMsg => sendMsg.delete({ timeout: 15000 }))
+  const targetId = (/^(?<targetId>\d{17,19})$/us).exec(message.content)?.groups?.targetId
 
-  return message.channel.awaitMessages((message: Message) => author.id === message.author.id, awaitMessageOptions)
-    .then(collected => collected.first() as Message)
-    .then(message => handleMessageHighlight(message))
-    .catch(() => message.reply('30秒経過してもIDが送信されなかったため、コレクターを終了しました。').then(sendMsg => sendMsg.delete({ timeout: 10000 })))
+  if (!targetId) return message.reply('ハイライトするメッセージのIDを送信してください、')
+    .then(() => awaitMessages(message))
+    .catch(reason => {
+      if (reason instanceof Collection) return message.reply('30秒経過してもIDが送信されないため、コレクターを終了しました。', { code: true })
+        .then(message => message.delete({ timeout: 10000 }))
+  
+      return message.reply(reason, { code: 'js' })
+    })
+
+  return message.channel.messages.fetch(targetId)
+    .then(target => sendHighlightedCode(target, message))
+    .catch(reason => message.reply(reason, { code: 'js' }))
 })
 
 client.login()
